9783cb9e3448a4ab78e51d2e2b76158ba937952d
devtrantrongtri <devtrantrongtri@gmail.com>
2024-11-20 14:52:33 +0700

    a

--- config/check_cookie.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-if (isset($_COOKIE['username'])) {
-    echo json_encode(["status" => "exists"]);
-} else {
-    echo json_encode(["status" => "missing"]);
-}
